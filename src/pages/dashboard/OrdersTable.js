a1e73e06ea58c1e0b6b0cf1150c49a31e62ebc14
Andrew Tate <tate@gmail.com>
2023-07-10 21:27:15 +0300

    applications list

--- src/pages/dashboard/OrdersTable.js
+++ src/pages/dashboard/OrdersTable.js
@@ -1,92 +1,107 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // material-ui
-import { Box, Link, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { IconButton, Box, Link, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
 
 // third-party
-import NumberFormat from 'react-number-format';
+// import NumberFormat from 'react-number-format';
 
 // project import
 import Dot from 'components/@extended/Dot';
 
-function createData(trackingNo, name, fat, carbs, protein) {
-  return { trackingNo, name, fat, carbs, protein };
-}
+// function descendingComparator(a, b, orderBy) {
+//   if (b[orderBy] < a[orderBy]) {
+//     return -1;
+//   }
+//   if (b[orderBy] > a[orderBy]) {
+//     return 1;
+//   }
+//   return 0;
+// }
 
-const rows = [
-  createData(84564564, 'Camera Lens', 40, 2, 40570),
-  createData(98764564, 'Laptop', 300, 0, 180139),
-  createData(98756325, 'Mobile', 355, 1, 90989),
-  createData(98652366, 'Handset', 50, 1, 10239),
-  createData(13286564, 'Computer Accessories', 100, 1, 83348),
-  createData(86739658, 'TV', 99, 0, 410780),
-  createData(13256498, 'Keyboard', 125, 2, 70999),
-  createData(98753263, 'Mouse', 89, 2, 10570),
-  createData(98753275, 'Desktop', 185, 1, 98063),
-  createData(98753291, 'Chair', 100, 0, 14001)
-];
+// function getComparator(order, orderBy) {
+//   return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+// }
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+// function stableSort(array, comparator) {
+//   const stabilizedThis = array.map((el, index) => [el, index]);
+//   stabilizedThis.sort((a, b) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) {
+//       return order;
+//     }
+//     return a[1] - b[1];
+//   });
+//   return stabilizedThis.map((el) => el[0]);
+// }
 
 // ==============================|| ORDER TABLE - HEADER CELL ||============================== //
 
 const headCells = [
   {
-    id: 'trackingNo',
+    id: 'id',
     align: 'left',
     disablePadding: false,
-    label: 'Tracking No.'
+    label: 'ID'
   },
   {
-    id: 'name',
+    id: 'from',
     align: 'left',
     disablePadding: true,
-    label: 'Product Name'
+    label: 'From'
   },
   {
-    id: 'fat',
-    align: 'right',
-    disablePadding: false,
-    label: 'Total Order'
-  },
-  {
-    id: 'carbs',
+    id: 'to',
     align: 'left',
     disablePadding: false,
-
+    label: 'To'
+  },
+  {
+    id: 'username',
+    align: 'left',
+    disablePadding: false,
+    label: 'Name'
+  },
+  {
+    id: 'tel',
+    align: 'left',
+    disablePadding: false,
+    label: 'Number'
+  },
+  {
+    id: 'carName',
+    align: 'left',
+    disablePadding: false,
+    label: 'Car name'
+  },
+  {
+    id: 'status',
+    align: 'left',
+    disablePadding: false,
     label: 'Status'
   },
   {
-    id: 'protein',
-    align: 'right',
+    id: 'actions',
+    align: 'left',
     disablePadding: false,
-    label: 'Total Amount'
+    label: ''
   }
 ];
 
@@ -123,18 +138,22 @@ const OrderStatus = ({ status }) => {
   let title;
 
   switch (status) {
-    case 0:
-      color = 'warning';
-      title = 'Pending';
+    case 'created':
+      color = 'primary';
+      title = 'Created';
       break;
-    case 1:
+    case 'approved':
       color = 'success';
       title = 'Approved';
       break;
-    case 2:
+    case 'declined':
       color = 'error';
-      title = 'Rejected';
+      title = 'Declined';
       break;
+      case 'waiting':
+        color = 'warning';
+        title = 'Waiting';
+        break;
     default:
       color = 'primary';
       title = 'None';
@@ -149,17 +168,34 @@ const OrderStatus = ({ status }) => {
 };
 
 OrderStatus.propTypes = {
-  status: PropTypes.number
+  status: PropTypes.string
 };
 
 // ==============================|| ORDER TABLE ||============================== //
+
 
 export default function OrderTable() {
   const [order] = useState('asc');
   const [orderBy] = useState('trackingNo');
   const [selected] = useState([]);
+  const [applications, setApplications] = useState([]);
 
   const isSelected = (trackingNo) => selected.indexOf(trackingNo) !== -1;
+
+  const getApplications = () => {
+    getDocs(collection(db, "ez"))
+      .then((querySnapshot)=>{               
+        const applications = querySnapshot.docs
+        .map((doc) => ({...doc.data(), id:doc.id }));
+        setApplications(applications);
+      });
+  };
+
+  console.log(applications)
+
+  React.useEffect(() => {
+    getApplications();
+  }, []);
 
   return (
     <Box>
@@ -186,7 +222,9 @@ export default function OrderTable() {
         >
           <OrderTableHead order={order} orderBy={orderBy} />
           <TableBody>
-            {stableSort(rows, getComparator(order, orderBy)).map((row, index) => {
+            {applications
+              .sort((a, b) => b.id - a.id)
+              .map((row, index) => {
               const isItemSelected = isSelected(row.trackingNo);
               const labelId = `enhanced-table-checkbox-${index}`;
 
@@ -197,21 +235,26 @@ export default function OrderTable() {
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   aria-checked={isItemSelected}
                   tabIndex={-1}
-                  key={row.trackingNo}
+                  key={row.id}
                   selected={isItemSelected}
                 >
                   <TableCell component="th" id={labelId} scope="row" align="left">
                     <Link color="secondary" component={RouterLink} to="">
-                      {row.trackingNo}
+                      {row.id}
                     </Link>
                   </TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="left">{row.from}</TableCell>
+                  <TableCell align="left">{row.to}</TableCell>
+                  <TableCell align="left">{row.username}</TableCell>
+                  <TableCell align="left">{row.tel}</TableCell>
+                  <TableCell align="left">{row.car}</TableCell>
                   <TableCell align="left">
-                    <OrderStatus status={row.carbs} />
+                    <OrderStatus status={row.status} />
                   </TableCell>
-                  <TableCell align="right">
-                    <NumberFormat value={row.protein} displayType="text" thousandSeparator prefix="$" />
+                  <TableCell align="left">
+                  <IconButton aria-label="edit application">
+                    <KeyboardArrowRightOutlinedIcon />
+                  </IconButton>
                   </TableCell>
                 </TableRow>
               );
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -27,7 +27,7 @@ const MainRoutes = {
           element: <>Documents</>
         }
       ]
-    },
+    }
   ]
 };
 
