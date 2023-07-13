bc9d5bcae1da3aadf7077155bc32abd76be899d5
Andrew Tate <tate@gmail.com>
2023-07-13 17:35:57 +0300

    save

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
 import {
+  Button,
   Grid,
   Typography,
   Box,
@@ -10,6 +11,7 @@ import {
   FormControl,
   InputLabel
 } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
 
@@ -31,8 +33,14 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export const DATE_FORMAT = 'DD.MM.YYYY';
+export const LOCATIONS_MAP = {
+  1: 'Riga, Valguma iela 4a',
+  2: 'Riga International Airport RIX'
+};
 
 const DatesLocations = ({ document, setInputData, inputData }) => {
+  if (!Object.keys(inputData).length) return null;
+
   return (
     <MainCard content={false} sx={{ padding: '8px 16px' }}>
       <Box
@@ -47,8 +55,8 @@ const DatesLocations = ({ document, setInputData, inputData }) => {
         <TextField
           disabled
           label="Contract number"
-          value={`RDV/${document.carId}_${document.id}`}
-          onChange={(e) => setInputData(oldData => ({ ...oldData, ['contractNumber']: e.target.value }))} />
+          value={inputData['contractNumber']}
+        />
         <DatePicker
           label="Pick up date"
           value={inputData['pickupDate'] ? moment(inputData['pickupDate'], DATE_FORMAT) : moment(document.from, DATE_FORMAT)}
@@ -64,11 +72,11 @@ const DatesLocations = ({ document, setInputData, inputData }) => {
         <FormControl>
           <InputLabel>Pick up location</InputLabel>
           <Select
-              value={inputData['pickupLocation'] || Number(document.delivery)}
+              value={inputData['pickupLocation'] || Number(document.delivery) || 1}
               onChange={e => setInputData(oldData => ({ ...oldData, ['pickupLocation']: e.target.value }))}
             >
-              <MenuItem value={1}>Valguma iela 4a-3 LV1048, Riga, Latvia</MenuItem>
-              <MenuItem value={2}>Airport</MenuItem>
+              <MenuItem value={1}>{LOCATIONS_MAP[1]}</MenuItem>
+              <MenuItem value={2}>{LOCATIONS_MAP[2]}</MenuItem>
               <MenuItem value={3}>Custom</MenuItem>
             </Select>
         </FormControl>
@@ -83,11 +91,11 @@ const DatesLocations = ({ document, setInputData, inputData }) => {
         <FormControl>
           <InputLabel>Drop off location</InputLabel>
           <Select
-              value={inputData['dropoffLocation'] || Number(document.admission)}
+              value={inputData['dropoffLocation'] || Number(document.admission) || 1}
               onChange={e => setInputData(oldData => ({ ...oldData, ['dropoffLocation']: e.target.value }))}
             >
-              <MenuItem value={1}>Valguma iela 4a-3 LV1048, Riga, Latvia</MenuItem>
-              <MenuItem value={2}>Airport</MenuItem>
+              <MenuItem value={1}>{LOCATIONS_MAP[1]}</MenuItem>
+              <MenuItem value={2}>{LOCATIONS_MAP[2]}</MenuItem>
               <MenuItem value={3}>Custom</MenuItem>
             </Select>
         </FormControl>
@@ -104,16 +112,47 @@ const DatesLocations = ({ document, setInputData, inputData }) => {
   );
 };
 
+const getPayloadLocations = (inputData) => {
+  const {
+    pickupLocation,
+    dropoffLocation,
+    customPickupLocation,
+    customDropoffLocation
+  } = inputData;
+
+  const pickupLocationText = pickupLocation === 3
+    ? customPickupLocation
+    : LOCATIONS_MAP[pickupLocation];
+  const dropoffLocationText = dropoffLocation === 3
+    ? customDropoffLocation
+    : LOCATIONS_MAP[dropoffLocation];
+
+  return {
+    pickupLocation: pickupLocationText,
+    dropoffLocation: dropoffLocationText
+  };
+};
+
 const AddPage = () => {
-  const [document, setDocument] = React.useState({});
   const { id } = useParams();
+  const [document, setDocument] = React.useState({});
   const [inputData, setInputData] = React.useState({});
 
   const getApplications = () => {
     getDocs(collection(db, "ez"))
       .then((querySnapshot) => {
         const applications = querySnapshot.docs.map((doc) => ({...doc.data(), id:doc.id }));
-        setDocument(applications.find(({ id: applicationId }) => id === applicationId));
+        const documentById = applications.find(({ id: applicationId }) => id === applicationId);
+        setDocument(documentById);
+        setInputData({
+          contractNumber: `RDV/${documentById.carId}_${documentById.id}`,
+          pickupDate: documentById.from,
+          dropoffDate: documentById.to,
+          pickupLocation: Number(documentById.delivery),
+          dropoffLocation: Number(documentById.admission),
+          customPickupLocation: '',
+          customDropoffLocation: ''
+        })
       });
   };
 
@@ -121,17 +160,40 @@ const AddPage = () => {
     getApplications();
   }, []);
 
-  console.log(inputData, document)
+  const onSaveClick = () => {
+    const locations = getPayloadLocations(inputData);
+
+    const payload = {
+      contractNumber: inputData['contractNumber'],
+      pickupDate: inputData['pickupDate'],
+      dropoffDate: inputData['dropoffDate'],
+      ...locations
+    };
+
+    console.log(payload)
+  };
 
   return (
     <Grid container rowSpacing={2.5}>
-      <Grid item>
-        <Typography variant="h3">Add document #{id}</Typography>
+      <Grid item xs={12} md={12} lg={12}>
+        <Grid container justifyContent="space-between">
+          <Typography variant="h3">Add document #{id}</Typography>
+          <Button
+            startIcon={<SaveIcon />}
+            color="success"
+            variant="contained"
+            onClick={() => onSaveClick()}
+          >
+            Save
+          </Button>
+        </Grid>
       </Grid>
       <Grid item xs={12} md={12} lg={12}>
         <MainCard content={false} sx={{ padding: '20px 16px' }}>
-          <Grid item xs={12} md={6} lg={3}>
-            <DatesLocations document={document} setInputData={setInputData} inputData={inputData} />
+          <Grid container>
+            <Grid item xs={12} md={6} lg={3}>
+              <DatesLocations document={document} setInputData={setInputData} inputData={inputData} />
+            </Grid>
           </Grid>
         </MainCard>
       </Grid>
